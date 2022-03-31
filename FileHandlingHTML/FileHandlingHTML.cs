30d9a054bfbb02634a8af5a5ca367fd3567d9086
Saumyadip Mukherjee <101113515+makorsha69@users.noreply.github.com>
2022-03-31 20:04:35 +0530

    Updated line number 17 and 21.

--- FileHandlingHTML/FileHandlingHTML.cs
+++ FileHandlingHTML/FileHandlingHTML.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,15 +14,15 @@ namespace FileHandlingHTML
             FileStream fileStream = new FileStream(@"C:\Training\mydata.txt", FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(fileStream);
             int count = 0;
+            string Word = "<input";
             while (reader.Peek() > 0)
             {
                 string line = reader.ReadLine();
-                if (line.Contains("<input"))
+                if (line.Contains(Word))
                 {
                     Console.WriteLine(line);
                     count++;
                 }
-                
             }
             Console.WriteLine("Number of <input = " + count);
             reader.Close();

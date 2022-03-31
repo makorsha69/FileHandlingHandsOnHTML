0b981bfd6b4236e5c966e3ce4c56d41409295adf
soumy <soumy@SAUMYADIP>
2022-03-31 18:41:23 +0530

    Add project files.

---
+++ FileHandlingHTML/FileHandlingHTML.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHandlingHTML
+{
+    internal class FileHandlingHTML
+    {
+        public void ReadFile1()
+        {
+            FileStream fileStream = new FileStream(@"C:\Training\mydata.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fileStream);
+            int count = 0;
+            while (reader.Peek() > 0)
+            {
+                string line = reader.ReadLine();
+                if (line.Contains("<input"))
+                {
+                    Console.WriteLine(line);
+                    count++;
+                }
+                
+            }
+            Console.WriteLine("Number of <input = " + count);
+            reader.Close();
+            fileStream.Close();
+        }
+    }
+}
---
+++ FileHandlingHTML/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FileHandlingHTML")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileHandlingHTML")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c6be8bb-8f71-43b0-9507-51c25ea79bfa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
